61b4281611b7d001a7d1de6231c052d21e178dd0
azzaahlem <bsAzzaahlem@gmail.com>
2024-05-26 10:52:48 +0200

    second

--- app/M/dashbord/[id]/page.jsx
+++ app/M/dashbord/[id]/page.jsx
@@ -1,6 +1,6 @@
 import { updateUser } from "/app/lib/actions";
-import { fetchUser } from "app/lib/data";
-import { fetchServices } from "app/lib/data";
+import { fetchUser } from "../../../../lib/data";
+import { fetchServices } from "../../../../lib/data";
 import styles from "/app/M/ui/dashbord/singleUser/singleUser.module.css";
 import Image from "next/image";
 
