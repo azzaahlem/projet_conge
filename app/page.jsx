ab60bd3a3402a17488c6b791a835314a3d3c53f4
azzaahlem <bsAzzaahlem@gmail.com>
2024-05-31 23:25:14 +0200

    ml

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 //import LoginForm from '/components/LoginForm';
 import { fetchUser } from '/app/lib/Data';
-import { fetchDemand } from './lib/data';
+import { fetchDemand } from '/app/lib/Data';
 import LoginPage from './login/page';
 
 
